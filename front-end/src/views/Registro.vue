bdf43e61de2376c85470be31b8a665917d428d1c
lucasnves <lucas.snowsg@gmail.com>
2022-11-01 19:08:38 -0300

    instalando banco de dados json, instalando vuelidate

--- front-end/src/views/Registro.vue
+++ front-end/src/views/Registro.vue
@@ -1,58 +1,58 @@
 <template>
-  <b-row class="vh-100 vw-100 cont">
-    <b-col sm="8" class="left-registro">
-      <h1>IMAGEM</h1>
-    </b-col>
+  <div>
+    <b-row class="vh-100 vw-100 cont">
+      <b-col sm="8" class="left-registro">
+        <h1>IMAGEM</h1>
+      </b-col>
 
-    <b-col sm="4" class="right-registro">
-      <form id="form-login">
-        <div class="registro-titulo">
-          <h1>Faça o cadastro</h1>
-        </div>
-        <div class="info-container">
-          <label for="name"><p>Nome</p></label>
-          <input type="text" name="name" id="name" placeholder="Lucas Neves" v-model="form.name">
-        </div>
-        <div class="info-container">
-          <label for="name"><p>Celular</p></label>
-          <input type="text" name="name" id="name" placeholder="(37)99976-8923" v-model="form.celular">
-        </div>
-        <div class="info-container">
-          <label for="name"><p>CPF</p></label>
-          <input type="text" name="name" id="name" placeholder="CPF" v-model="form.cpf">
-        </div>
-        <div class="info-container">
-          <label for="email"><p>Email</p></label>
-          <input type="email" name="email" id="email" placeholder="lucas.gabriel@gmail.com" v-model="form.email">
-        </div>
-        <div class="info-container">
-          <label for="password"><p>Senha</p></label>
-          <input type="password" name="password" id="password" placeholder="•••••••••••" v-model="form.password">
-        </div>
-        <div class="info-container">
-          <label for="passwordConfirm"><p>Confirme a senha</p></label>
-          <input type="password" name="passwordConfirm" id="passwordConfirm" placeholder="•••••••••••" v-model="form.passwordConfirm">
-        </div>
+      <b-col sm="4" class="right-registro">
+        <form id="form-login" @submit="cadastrar">
+          <div class="registro-titulo">
+            <h1>Faça o cadastro</h1>
+          </div>
+          <div class="info-container">
+            <label for="nome"><p>Nome</p></label>
+            <input type="text" name="nome" id="nome" placeholder="Lucas Neves" v-model="form.name">
+          </div>
+          <div class="info-container">
+            <label for="celular"><p>Celular</p></label>
+            <input type="text" name="celular" id="celular" placeholder="(37)99976-8923" v-model="form.celular">
+          </div>
+          <div class="info-container">
+            <label for="cpf"><p>CPF</p></label>
+            <input type="text" name="cpf" id="cpf" placeholder="CPF" v-model="form.cpf">
+          </div>
+          <div class="info-container">
+            <label for="email"><p>Email</p></label>
+            <input type="email" name="email" id="email" placeholder="lucas.gabriel@gmail.com" v-model="form.email">
+          </div>
+          <div class="info-container">
+            <label for="password"><p>Senha</p></label>
+            <input type="password" name="password" id="password" placeholder="•••••••••••" v-model="form.password">
+          </div>
+          <div class="info-container">
+            <label for="passwordConfirm"><p>Confirme a senha</p></label>
+            <input type="password" name="passwordConfirm" id="passwordConfirm" placeholder="•••••••••••" v-model="form.passwordConfirm">
+          </div>
 
-        <div class="info-container">
-          <b-button
-            type="button"
-            block
-            class="mb-2"
-            @click="cadastrar"
-          >Confirmar
-          </b-button>
-          <b-button
-            type="button"
-            block
-            to="/login"
-          >Voltar
-          </b-button>
-        </div>
-
-      </form>
-    </b-col>
-  </b-row>
+          <div class="info-container">
+            <b-button
+              type="submit"
+              block
+              class="mb-2"
+            >Confirmar
+            </b-button>
+            <b-button
+              type="button"
+              block
+              to="/login"
+            >Voltar
+            </b-button>
+          </div>
+        </form>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
@@ -71,7 +71,29 @@ export default {
     }
   },
   methods: {
-    cadastrar() {
+    async cadastrar(e) {
+      e.preventDefault();
+
+      const data = {
+        nome: this.form.name,
+        celular: this.form.celular,
+        cpf: this.form.cpf,
+        email: this.form.email,
+        senha: this.form.password,
+        senhaConfirmar: this.form.passwordConfirm
+      }
+
+      //transforma os dados em uma string para mandar para o banco
+      const data_string = JSON.stringify(data);
+
+      //salvando no banco
+      const req = await fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: data_string
+      });
+
+      //enviando a rota para login
       this.$router.push({ name: 'login' });
     }
   }
